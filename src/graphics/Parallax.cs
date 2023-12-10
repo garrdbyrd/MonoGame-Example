38e924f7a9d126dcbbd54f8db6bf9b4dba7cbea9
Garrett Byrd <gdb070@yahoo.com>
2023-12-10 16:25:58 -0500

    Groundwork for Parallax class

---
+++ src/graphics/Parallax.cs
@@ -0,0 +1,34 @@
+//  Parallax
+//  classes:
+//    - Layer(graphics (.bmp, .png? .tif?), depth (float), speed (float))
+//        - Init position (Vector2) to (0f,0f)
+//  methods:
+//    - void Add(Layer)
+//    - void Remove(Layer)
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualBasic.FileIO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Pevensie;
+
+public class Parallax
+{
+    private List<Layer> Layers = new List<Layer>();
+    public class Layer
+    {
+        public string ID { get; set; }
+        public Texture2D Texture { get; set; }
+        public float Depth { get; set; }
+        public Vector2 Position { get; set; } = new Vector2(0f, 0f);
+        public float speedScalar { get; set; } = 1f;
+    }
+    public void AddLayer(Parallax.Layer newLayer)
+    {
+        Layers.Add(newLayer);
+    }
+}
+
+
