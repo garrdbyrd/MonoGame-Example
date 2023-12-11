e21f3580708acf10ecdbb0eb2daa9bcc5aa0f43d
Garrett Byrd <gdb070@yahoo.com>
2023-12-10 23:45:28 -0500

    Cleaned up GraphicsSettings

--- src/graphics/GraphicsSettings.cs
+++ src/graphics/GraphicsSettings.cs
@@ -12,26 +12,26 @@ public class GraphicsSettings
 
     public class GraphicsObject
     {
-        public int screenWidth { get; set; }
-        public int screenHeight { get; set; }
-        public bool fullscreen { get; set; }
-        public bool vsync { get; set; }
+        public int ScreenWidth { get; set; }
+        public int ScreenHeight { get; set; }
+        public bool Fullscreen { get; set; }
+        public bool Vsync { get; set; }
 
         public GraphicsObject(int screenWidth, int screenHeight, bool fullscreen, bool vsync)
         {
-            this.screenWidth = screenWidth;
-            this.screenHeight = screenHeight;
-            this.fullscreen = fullscreen;
-            this.vsync = vsync;
+            ScreenWidth = screenWidth;
+            ScreenHeight = screenHeight;
+            Fullscreen = fullscreen;
+            Vsync = vsync;
         }
     }
 
     public void SetGraphics(GraphicsObject Settings)
     {
-        this._graphics.PreferredBackBufferWidth = Settings.screenWidth;
-        this._graphics.PreferredBackBufferHeight = Settings.screenHeight;
-        this._graphics.IsFullScreen = Settings.fullscreen;
-        this._graphics.SynchronizeWithVerticalRetrace = Settings.vsync;
+        this._graphics.PreferredBackBufferWidth = Settings.ScreenWidth;
+        this._graphics.PreferredBackBufferHeight = Settings.ScreenHeight;
+        this._graphics.IsFullScreen = Settings.Fullscreen;
+        this._graphics.SynchronizeWithVerticalRetrace = Settings.Vsync;
         this._graphics.ApplyChanges();
     }
 }
